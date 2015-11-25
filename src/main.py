ae8082d4aaa1d5b4251e2975ec3d88593cc43876
s4553711 <s4553711@gmail.com>
2015-11-25 23:31:09 +0800

    Add submodule of drmaa-python and import example

--- src/main.py
+++ src/main.py
@@ -5,6 +5,9 @@ import sys
 import pipeline
 #import QC
 #import Bwa
+lib_path = os.path.abspath(os.path.join('drmaa-python'))
+sys.path.append(lib_path)
+import drmaa
 
 p = pipeline.pipeline();
 p.clean()
